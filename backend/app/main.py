5c3c2b6c77b6166b73a0a7a161bc8400b1f9edb6
Asvitha20 <asvitharamesh20@gmail.com>
2026-08-01 06:13:05 +0530

    backend - CROS policy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 
 from app.routes.datasets import router as datasets_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="MoSPI API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:1313",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
